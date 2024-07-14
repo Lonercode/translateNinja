d25c932e200907fbbc206020324a85a59c647060
lonercode <penpalclassmate@gmail.com>
2024-07-14 08:05:16 +0100

    audio modification

--- static/scripty.js
+++ static/scripty.js
@@ -5,3 +5,13 @@ a.onkeydown = function(e) {
     }
 
 };
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register("/sw.js").then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
--- translate.py
+++ translate.py
@@ -1,20 +1,35 @@
+import os
 from os import error
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, send_file
 from flask_googletrans import translator
+import gtts.lang
+from playsound import playsound
+import time
+from gtts import gTTS
+import gtts
+
+
 
 app = Flask(__name__)
 
 ts = translator(app)
 
 
+@app.route('/manifest.json')
+def serve_manifest():
+    return send_file('manifest.json', mimetype='application/manifest+json')
 
+
+@app.route('/sw.js')
+def serve_sw():
+    return send_file('sw.js', mimetype='application/javascript')
 
 @app.route('/', methods=["POST", "GET"])
 def home():
     if request.method=="POST":
         try:
             text = request.form["area"]
-        
+
             textb = request.form["them"]
             textc = request.form["them1"]
             return redirect(url_for("text", txt = text, sct = textb, sct1 = textc))
@@ -31,6 +46,13 @@ def home():
 
 @app.route('/<txt>/<sct>/<sct1>')
 def text(txt, sct, sct1):
+    if sct1 in gtts.lang.tts_langs().keys():
+        aud = gTTS(text=ts.translate(text=txt, src=sct, dest=[sct1]), lang=sct1)
+        aud.save('./static/translated.mp3')
+    else:
+        ts.translate(text=txt, src=sct, dest=[sct1])
+        aud = gTTS(text = "I am sorry, but there is no audio for this language", lang = 'en')
+        aud.save('./static/translated.mp3')
     return render_template('trans.html', text = txt, textb = sct, textc = sct1)
 
 
